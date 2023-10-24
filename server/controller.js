27877daa78aa279b8c3276f8e6cdfa82cb5fdaf8
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2023-10-24 14:18:06 -0600

    1st new feature added

--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,15 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = ["A faithful friend is a strong defense.","All will go well with your new project.","Long life is in store for you.","Now is the time to try something new.","You will be successful in your work."];
+
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+
+        res.status(200).send(randomFortune);
     }
 
 }
